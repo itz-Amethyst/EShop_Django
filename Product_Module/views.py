67affe1b21bbd0ab1e4e689dcf3be0516a6f2ab5
itz-amethyst <pransermi@gmail.com>
2023-08-19 21:12:42 +0330

    feat: Configured basic urls and views

--- Product_Module/views.py
+++ Product_Module/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def Product_List(request):
+    products = Product.objects.all()
+    return render(request, "Product_Module/product_list.html",{
+        'products': products
+    })

0c3444689908d90e36779093888a61bd9e59f561
itz-amethyst <pransermi@gmail.com>
2023-08-19 18:32:03 +0330

    feat: Added some model props and validations

--- Product_Module/models.py
+++ Product_Module/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length = 200)
     price = models.IntegerField()
+    rating = models.IntegerField(
+        validators = [MinValueValidator(1), MaxValueValidator(5)], default = 0
+    )
+    short_description = models.CharField(max_length = 360, null = True)
+    is_active = models.BooleanField(null = True)
+
+    def __str__(self):
+        return f"{self.title} ({self.price})"

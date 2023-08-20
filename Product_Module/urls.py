cbed21bd6db4ad48cf54a3bccf2fe75fbca8c70f
itz-amethyst <pransermi@gmail.com>
2023-08-20 18:34:16 +0330

    feat: configured product_details views

--- Product_Module/urls.py
+++ Product_Module/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Product_List)
+    path('', views.Product_List),
+    path('<id:int>', views.Product_Detail)
 ]
--- Product_Module/views.py
+++ Product_Module/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from .models import Product
+from django.http import Http404
 
 # Create your views here.
 
@@ -7,4 +8,17 @@ def Product_List(request):
     products = Product.objects.all()
     return render(request, "Product_Module/product_list.html",{
         'products': products
+    })
+
+def Product_Detail(request, id):
+    # try:
+    #     product = Product.objects.get(id = product_id)
+    # except:
+    #     raise Http404()
+
+    ## Same as Top
+    product = get_object_or_404(Product, pk = id)
+
+    return render(request, "Product_Module/product_details.html",{
+        'product': product
     })

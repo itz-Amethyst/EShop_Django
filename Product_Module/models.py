c8970b3d6f3a7c13cbe1a0ef7b09b4a2fc7b1067
itz-amethyst <pransermi@gmail.com>
2023-08-20 18:49:46 +0330

    feat: Added absolute url and fix

--- Product_Module/models.py
+++ Product_Module/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
@@ -11,5 +12,7 @@ class Product(models.Model):
     short_description = models.CharField(max_length = 360, null = True)
     is_active = models.BooleanField(null = True)
 
+    def get_absolute_url( self ):
+        return reverse('product-detail', args = [self.id])
     def __str__(self):
         return f"{self.title} ({self.price})"
--- Product_Module/views.py
+++ Product_Module/views.py
@@ -10,14 +10,14 @@ def Product_List(request):
         'products': products
     })
 
-def Product_Detail(request, id):
+def Product_Detail(request, product_id):
     # try:
     #     product = Product.objects.get(id = product_id)
     # except:
     #     raise Http404()
 
     ## Same as Top
-    product = get_object_or_404(Product, pk = id)
+    product = get_object_or_404(Product, pk = product_id)
 
     return render(request, "Product_Module/product_details.html",{
         'product': product

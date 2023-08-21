68f2f8f69f4f6238ea8e437920fed0a2d4fa59f2
itz-amethyst <pransermi@gmail.com>
2023-08-21 17:25:22 +0330

    feat: Looked at the admin page of site for the first time

--- EShop_Project/settings.py
+++ EShop_Project/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us' ## fa-ir Will change admin language to Persian !!
 
 TIME_ZONE = 'UTC'
 

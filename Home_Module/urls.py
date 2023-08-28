bb32d4340c3bd2ba1ed46d6f1f211172149e8ff9
itz-amethyst <pransermi@gmail.com>
2023-08-28 22:48:04 +0330

    feat: Added new views

--- Home_Module/urls.py
+++ Home_Module/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index_page),
+    path('contact-us', views.contact_page),
+    # path('site-header', views.site_header_partial, name="site_header_partial")
 ]
--- Home_Module/views.py
+++ Home_Module/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'Home_Module/index.html')
+
+def contact_page(request):
+    return render(request, 'Home_Module/contact-us.html')
+
+def site_header_component(request):
+    return render(request, 'shared/components/header.html', {})
+
+def site_footer_component(request):
+    return render(request, 'shared/components/footer.html', {})

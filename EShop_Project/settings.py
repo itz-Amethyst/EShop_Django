345d59b61af0c11ff33688a6d8f3ac2d612f87d5
itz-amethyst <pransermi@gmail.com>
2023-08-28 13:29:03 +0330

    feat: Layout index and settings configured

--- EShop_Project/settings.py
+++ EShop_Project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Product_Module'
+    'Product_Module',
+    'Home_Module'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

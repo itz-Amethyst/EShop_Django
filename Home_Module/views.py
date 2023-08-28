fb07d8ee9296644d47ae7521ea0dae56906992fa
itz-amethyst <pransermi@gmail.com>
2023-08-28 22:51:45 +0330

    feat: Tested something

--- Home_Module/views.py
+++ Home_Module/views.py
@@ -9,7 +9,9 @@ def contact_page(request):
     return render(request, 'Home_Module/contact-us.html')
 
 def site_header_component(request):
-    return render(request, 'shared/components/header.html', {})
+    return render(request, 'shared/components/header.html', {
+        'link': 'آموزش پایتون'
+    })
 
 def site_footer_component(request):
     return render(request, 'shared/components/footer.html', {})

363348d2cce63cd030984610140d92577705d24a
itz-amethyst <pransermi@gmail.com>
2023-08-29 18:52:52 +0330

    feat: Added csrf token for contact-us request

--- contact_module/views.py
+++ contact_module/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 
+from django.urls import reverse
 # Create your views here.
 
 def contact_us_page(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect(reverse('home_page'))
+
     return render(request , "contact_module/contact-us.html")

9d9b9093d015f80d1b3607f9ce0ed01b81ee4e1c
itz-amethyst <pransermi@gmail.com>
2023-08-30 14:06:35 +0330

    feat: Configured to show datas in forms class

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,9 +2,32 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField(label = "نام و نام خانوادگی", max_length = 50, error_messages = {
-        'required': 'لطفا نام و نام خواندگی را وارد کنید'
-    })
-    subject = forms.CharField(label = 'عنوان')
-    email = forms.EmailField(label = 'ایمیل' , required = False)
-    text = forms.CharField(widget = forms.Textarea, label = 'متن پیام')
+    full_name = forms.CharField(
+        label = "نام و نام خانوادگی",
+        max_length = 50,
+        error_messages = {
+        'required': 'لطفا نام و نام خواندگی را وارد کنید',
+        'max_lenght': 'نام و نام خانوادگی نمیتواند بیشتر از 50 کاراکتر باشد'
+        },
+        widget = forms.TextInput(attrs = {
+            'class': 'form-control',
+            'placeholder': 'نام و نام خانوادگی'
+        })
+
+    )
+    subject = forms.CharField(label = 'عنوان',
+            widget = forms.TextInput(attrs = {
+            'class': 'form-control',
+            'placeholder': 'عنوان'
+        }))
+    email = forms.EmailField(label = 'ایمیل', required = False, widget = forms.EmailInput(attrs = {
+            'class': 'form-control',
+            'placeholder': 'ایمیل '
+        })
+    )
+    text = forms.CharField(label = 'متن پیام',widget = forms.Textarea(attrs = {
+        'class': 'form-control',
+        'placeholder': 'متن پیام',
+        'rows': '5',
+        'id': 'message'
+    }))
--- contact_module/views.py
+++ contact_module/views.py
@@ -18,7 +18,9 @@ def contact_us_page(request):
         if contact_form.is_valid():
             print(contact_form.cleaned_data)
             return redirect('home_page')
+    else:
+        contact_form = ContactUsForm()
 
-    contact_form = ContactUsForm()
-
-    return render(request , "contact_module/contact-us.html")
+    return render(request, "contact_module/contact-us.html", {
+        'contact_form': contact_form
+    })

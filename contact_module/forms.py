a77a8106b9acd85d0558153e9349d9220d069dae
itz-amethyst <pransermi@gmail.com>
2023-08-30 13:20:46 +0330

    feat: Added more validations form

--- contact_module/forms.py
+++ contact_module/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    full_name = forms.CharField(label = "نام و نام خانوادگی")
-    subject = forms.CharField()
-    email = forms.EmailField()
-    text = forms.CharField(widget = forms.Textarea)
+    full_name = forms.CharField(label = "نام و نام خانوادگی", max_length = 50, error_messages = {
+        'required': 'لطفا نام و نام خواندگی را وارد کنید'
+    })
+    subject = forms.CharField(label = 'عنوان')
+    email = forms.EmailField(label = 'ایمیل' , required = False)
+    text = forms.CharField(widget = forms.Textarea, label = 'متن پیام')

375fdefa5d1e224d6f068c34bc2da7f621b664a0
itz-amethyst <pransermi@gmail.com>
2023-09-02 13:08:49 +0330

    feat: Changed to class view

--- contact_module/urls.py
+++ contact_module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact_us_page, name='contact_us')
+    #! Function based view
+    # path('', views.contact_us_page, name='contact_us')
+    path('', views.ContactUsView.as_view(), name='contact_us')
 ]
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from .forms import ContactUsForm , ContactUsModelForm
 from django.views import View
+from django.views.generic import ListView
 from django.urls import reverse
 
 from .models import ContactUs
@@ -27,24 +28,25 @@ class ContactUsView(View):
         })
 
 
-def contact_us_page(request):
-    #? Old Way
-    if request.method == "POST":
-        contact_form = ContactUsForm(request.POST)
-        if contact_form.is_valid():
-            print(contact_form.cleaned_data)
-            contact = ContactUs(
-                title = contact_form.cleaned_data.get('title'),
-                full_name = contact_form.cleaned_data.get('full_name'),
-                email = contact_form.cleaned_data.get('email'),
-                message = contact_form.cleaned_data.get('message'),
-            )
-
-            contact.save()
-            return redirect('home_page')
-    else:
-        contact_form = ContactUsForm()
-
-    return render(request, "contact_module/contact-us.html", {
-        'contact_form': contact_form
-    })
+#? Function based view
+# def contact_us_page(request):
+#     #? Old Way
+#     if request.method == "POST":
+#         contact_form = ContactUsForm(request.POST)
+#         if contact_form.is_valid():
+#             print(contact_form.cleaned_data)
+#             contact = ContactUs(
+#                 title = contact_form.cleaned_data.get('title'),
+#                 full_name = contact_form.cleaned_data.get('full_name'),
+#                 email = contact_form.cleaned_data.get('email'),
+#                 message = contact_form.cleaned_data.get('message'),
+#             )
+#
+#             contact.save()
+#             return redirect('home_page')
+#     else:
+#         contact_form = ContactUsForm()
+#
+#     return render(request, "contact_module/contact-us.html", {
+#         'contact_form': contact_form
+#     })

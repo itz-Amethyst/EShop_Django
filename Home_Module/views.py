037f2105e90d202fc91ad9cf89d5460f8fba2d43
itz-amethyst <pransermi@gmail.com>
2023-09-02 13:05:14 +0330

    feat: Added class based view in home page view

--- Home_Module/views.py
+++ Home_Module/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic.base import TemplateView
+
 
 # Create your views here.
+# class HomeView(View):
+#     def get( self, request ):
+#         return render(request, 'Home_Module/index.html')
 
-def index_page(request):
-    return render(request, 'Home_Module/index.html')
+class HomeView(TemplateView):
+    template_name = 'Home_Module/index.html'
 
-def contact_page(request):
-    return render(request, 'Home_Module/contact-us.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['data'] = 'this is data in home page'
+        return context
 
 def site_header_component(request):
     return render(request, 'shared/components/header.html', {

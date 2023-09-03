a0edea34f8397c58ffcb4f5e62df98c632da0117
itz-amethyst <pransermi@gmail.com>
2023-09-03 17:50:13 +0330

    feat: Configured view to show all profiles

--- contact_module/urls.py
+++ contact_module/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     #! Function based view
     # path('', views.contact_us_page, name='contact_us')
     path('', views.ContactUsView.as_view(), name='contact_us'),
-    path('create-profile/', views.CreateProfileView.as_view() , name = 'create_profile')
+    path('create-profile/', views.CreateProfileView.as_view() , name = 'create_profile'),
+    path('profiles/' , views.ProfilesView.as_view() , name = 'profiles')
 
 ]
--- contact_module/views.py
+++ contact_module/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render , redirect
-from .forms import ContactUsForm , ContactUsModelForm , ProfileForm
+from .forms import ContactUsForm , ContactUsModelForm
 from django.views import View
 from django.views.generic import ListView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView , CreateView
 from django.urls import reverse
 
 from .models import ContactUs , UserProfile
@@ -11,6 +11,7 @@ from .models import ContactUs , UserProfile
 # Create your views here.
 
 class ContactUsView(FormView):
+    form_class = ContactUsModelForm
     model = ContactUs
     template_name = 'contact_module/contact-us.html'
     success_url = '/contact-us/'
@@ -29,25 +30,37 @@ class ContactUsView(FormView):
     #         'contact_form': contact_form
     #     })
 
-class CreateProfileView(View):
-    def get( self, request ):
-        form = ProfileForm
-        return render(request, 'contact_module/create-profile.html',{
-            'form': form
-        })
+class CreateProfileView(CreateView):
+    template_name = 'contact_module/create-profile.html'
+    model = UserProfile
+    fields = '__all__'
+    success_url = '/contact-us/create-profile'
+    # success_url = 'contact-us/create-profile'
 
-    def post( self, request ):
-        submitted_form = ProfileForm(request.POST, request.FILES)
+    # def get( self, request ):
+    #     form = ProfileForm
+    #     return render(request, 'contact_module/create-profile.html',{
+    #         'form': form
+    #     })
+    #
+    # def post( self, request ):
+    #     submitted_form = ProfileForm(request.POST, request.FILES)
+    #
+    #     if submitted_form.is_valid():
+    #         print(request.FILES)
+    #         profile = UserProfile(image = request.FILES['user_image'])
+    #         profile.save()
+    #         return redirect('/contact-us/create-profile')
+    #
+    #     return render(request , 'contact_module/create-profile.html' , {
+    #         'form': submitted_form
+    #     })
 
-        if submitted_form.is_valid():
-            print(request.FILES)
-            profile = UserProfile(image = request.FILES['user_image'])
-            profile.save()
-            return redirect('/contact-us/create-profile')
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = 'contact_module/profile_list.html'
+    context_object_name = 'profiles'
 
-        return render(request , 'contact_module/create-profile.html' , {
-            'form': submitted_form
-        })
 #? Function based view
 # def contact_us_page(request):
 #     #? Old Way

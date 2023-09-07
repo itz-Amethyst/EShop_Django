2ada83e1b8c0367ebb37b243b806fbd305e19065
itz-amethyst <pransermi@gmail.com>
2023-09-07 23:01:57 +0330

    fix

--- Product_Module/urls.py
+++ Product_Module/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path , re_path
 from . import views
 
 urlpatterns = [
     # path('', views.Product_List, name='product-list'),
     path('', views.ProductListView.as_view(), name = 'product-list') ,
-    path('<product_favorite>' , views.AddProductFavoriteView.as_view() , name = 'product-favorite') ,
-    path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('product_favorite', views.AddProductFavoriteView.as_view() , name = 'product-favorite') ,
+    # path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
+    # re_path(r'products/(?P<slug>[-\w]+)/', views.ProductDetailView.as_view(), name='product-detail'),
+    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]

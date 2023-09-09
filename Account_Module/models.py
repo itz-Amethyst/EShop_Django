87b1057687395b98d258d221fc4674e2de513d4f
itz-amethyst <pransermi@gmail.com>
2023-09-09 12:06:14 +0330

    feat: added account_module

---
+++ Account_Module/models.py
@@ -0,0 +1,20 @@
+import django
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+# Create your models here.
+
+class User(AbstractUser):
+    mobile = models.CharField(max_length = 15, verbose_name = "تلفن همراه")
+    email_active_code = models.CharField(max_length = 100, verbose_name = "کد فعال سازی ایمیل")
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+
+    def __str__(self):
+        if (self.get_full_name() != None):
+            return self.get_full_name()
+
+        return self.email
+
+

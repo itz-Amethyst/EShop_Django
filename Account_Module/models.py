f5ac26e4eb8f7b33aaf1741603567cd7e8bbdb7e
itz-amethyst <pransermi@gmail.com>
2023-09-10 23:43:02 +0330

    feat: added basics of login

--- Account_Module/models.py
+++ Account_Module/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class User(AbstractUser):
-    mobile = models.CharField(max_length = 15, verbose_name = "تلفن همراه")
+    # mobile = models.CharField(max_length = 15, verbose_name = "تلفن همراه", null = True)
+    avatar = models.ImageField(upload_to = 'UserImages', verbose_name = 'تصویر کاربری', null = True, blank = True)
     email_active_code = models.CharField(max_length = 100, verbose_name = "کد فعال سازی ایمیل")
 
     class Meta:

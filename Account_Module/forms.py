7ac16355bdae3dd957bcc5a5ab2e72b3e6571b47
itz-amethyst <pransermi@gmail.com>
2023-09-11 12:38:18 +0330

    feat: Added login form

--- Account_Module/forms.py
+++ Account_Module/forms.py
@@ -2,31 +2,58 @@ from django import forms
 from django.core import validators
 from django.core.exceptions import ValidationError
 
-
 class RegisterForm(forms.Form):
+    email = forms.EmailField(
+        label = 'ایمیل' ,
+        widget = forms.EmailInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+            validators.EmailValidator
+        ]
+    )
 
-   email = forms.EmailField(
-       label = 'ایمیل',
-       widget = forms.EmailInput(),
-       validators = [
-           validators.MaxLengthValidator(100),
-           validators.EmailValidator
-       ]
-   )
+    password = forms.CharField(
+        label = 'کلمه عبور' ,
+        widget = forms.PasswordInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+        ]
+    )
+    confirm_password = forms.CharField(
+        label = 'تکرار کلمه عبور' ,
+        widget = forms.PasswordInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+        ]
+    )
 
-   password = forms.CharField(
-       label = 'کلمه عبور',
-       widget = forms.PasswordInput()
-   )
-   confirm_password = forms.CharField(
-       label = 'تکرار کلمه عبور',
-       widget = forms.PasswordInput()
-   )
+    def clean_confirm_password( self ):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
 
-   def clean_confirm_password(self):
-       password = self.cleaned_data.get('password')
-       confirm_password= self.cleaned_data.get('confirm_password')
+        if password == confirm_password:
+            return confirm_password
+        raise ValidationError('کلمه عبور با تکرار ان یکسان نیست')
 
-       if password == confirm_password:
-           return confirm_password
-       raise ValidationError('کلمه عبور با تکرار ان یکسان نیست')
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        label = 'ایمیل' ,
+        widget = forms.EmailInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+            validators.EmailValidator
+        ]
+    )
+
+    password = forms.CharField(
+        label = 'کلمه عبور' ,
+        widget = forms.PasswordInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+        ]
+    )
+
+
+# ! Use modelForm when you have fully customizable model because it will read validations and stuffs from model
+

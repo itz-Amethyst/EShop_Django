c6c45ced1679db5448d6dd9ea89f841a87948dae
itz-amethyst <pransermi@gmail.com>
2023-09-11 14:40:07 +0330

    feat: added reset_password form

--- Account_Module/forms.py
+++ Account_Module/forms.py
@@ -83,3 +83,32 @@ class Forget_Password_Form(forms.Form):
 
 
 
+
+
+class Reset_Password_Form(forms.Form):
+    password = forms.CharField(
+        label = 'کلمه عبور' ,
+        widget = forms.PasswordInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+        ]
+    )
+    confirm_password = forms.CharField(
+        label = 'تکرار کلمه عبور' ,
+        widget = forms.PasswordInput() ,
+        validators = [
+            validators.MaxLengthValidator(100) ,
+        ]
+    )
+
+    def clean_confirm_password( self ):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+
+        if password == confirm_password:
+            return confirm_password
+        raise ValidationError('کلمه عبور با تکرار ان یکسان نیست')
+    def __init__( self , *args , **kwargs ):
+        super(Reset_Password_Form , self).__init__(*args , **kwargs)
+        add_form_control_to_fields(self)
+

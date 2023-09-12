78d9b8bdbf8e92396e54914127817ff299e6063f
itz-amethyst <pransermi@gmail.com>
2023-09-12 17:56:03 +0330

    feat: Initialized site setting in view home module

--- Home_Module/views.py
+++ Home_Module/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 
+from Site_Module.models import SiteSetting
+
 
 # Create your views here.
 # class HomeView(View):
@@ -19,5 +21,18 @@ class HomeView(TemplateView):
 
 class Site_Header_Component_View(TemplateView):
     template_name = 'shared/components/header.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        setting: SiteSetting = SiteSetting.objects.filter(is_main_setting = True).first()
+        context['site_setting'] = setting
+        return context
+
 class Site_Footer_Component_View(TemplateView):
     template_name = 'shared/components/footer.html'
+
+    def get_context_data( self , **kwargs ):
+        context = super().get_context_data()
+        setting: SiteSetting = SiteSetting.objects.filter(is_main_setting = True).first()
+        context['site_setting'] = setting
+        return context

c25006b344f026152b4f4fa05c5edb6d6879bed5
itz-amethyst <pransermi@gmail.com>
2023-09-12 12:28:08 +0330

    feat: Added Site_Settings model and admin

--- Site_Module/apps.py
+++ Site_Module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SiteModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Site_Module'
+    verbose_name = 'ماژول تنظیمات سایت'
--- Site_Module/models.py
+++ Site_Module/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class SiteSetting(models.Model):
+    is_main_setting = models.BooleanField(verbose_name = 'تنظیمات اصلی سایت')
+    site_name = models.CharField(max_length = 200, verbose_name = 'نام سایت')
+    site_url = models.CharField(max_length = 200, verbose_name = 'دامنه سایت')
+    address = models.CharField(max_length = 200, verbose_name = 'ادرس')
+    phone = models.CharField(max_length = 200, null = True, blank = True, verbose_name = 'تلفن')
+    fax = models.CharField(max_length = 200, null = True, blank = True, verbose_name = 'فکس')
+    email = models.CharField(max_length = 200, null = True, blank = True, verbose_name = 'ایمیل')
+    site_logo = models.ImageField(upload_to = 'images/site-settings/' , verbose_name = 'لوگو سایت')
+    copy_right = models.TextField(verbose_name = 'متن کپی رایت سایت')
+    about_us_text = models.TextField(max_length = 200, verbose_name = 'متن درباره ما سایت')
+
+    class Meta:
+        verbose_name = 'تنظیمات سایت'
+        verbose_name_plural = 'تنظیمات'
+
+    def __str__( self ):
+        return self.site_name

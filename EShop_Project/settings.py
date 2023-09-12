db9a654fa5c6f2cf13f905f3e27780e8b4c96f23
itz-amethyst <pransermi@gmail.com>
2023-09-12 11:12:34 +0330

    feat: configured urls and passwords for email

--- EShop_Project/settings.py
+++ EShop_Project/settings.py
@@ -150,6 +150,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 # UserName Password Gmail
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'pransermi@gmail.com'
+EMAIL_HOST_PASSWORD = 'fygrifnzzojkwlse'
 EMAIL_PORT = 587

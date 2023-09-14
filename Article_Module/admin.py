3905d4b8202ae3e88b8c70ec6616a7f52ac1ede8
itz-amethyst <pransermi@gmail.com>
2023-09-14 18:08:35 +0330

    feat: Configured article admin to save author

--- Article_Module/admin.py
+++ Article_Module/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from . import models
+from .models import Article
 
 
 # Register your models here.
@@ -8,10 +9,22 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     list_editable = ['url_title', 'parent', 'is_active']
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'make_short_description_shorter', 'is_active', 'get_selected_categories']
+    list_display = ['title', 'make_short_description_shorter', 'is_active', 'get_selected_categories', 'author']
     list_editable = ['is_active']
 
-    # Note in admin it will display the name of def
+    def save_model(self, request, obj: Article, form, change):
+
+        print('change:', change)
+        print('object:', obj)
+        print('user:', request.user)
+
+        if not change:
+            #? Note only change author in create page or action not in edit f.e created by milad edited by reza but author still is milad
+            obj.author = request.user
+
+        # obj.author = request.user
+        return super().save_model(request, obj, form, change)
+    #! Note in admin it will display the name of def
     def make_short_description_shorter( self, obj ):
         return obj.short_description[:65]
 

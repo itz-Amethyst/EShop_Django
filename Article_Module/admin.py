d177c6a274a19e5bab96db4f7f158fb86debe6f9
itz-amethyst <pransermi@gmail.com>
2023-09-14 15:58:15 +0330

    feat: Added Article model and admin of itself

--- Article_Module/admin.py
+++ Article_Module/admin.py
@@ -7,5 +7,13 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'url_title', 'parent', 'is_active']
     list_editable = ['url_title', 'parent', 'is_active']
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'make_short_description_shorter', 'is_active', 'get_selected_categories']
+
+    # Note in admin it will display the name of def
+    def make_short_description_shorter( self, obj ):
+        return obj.short_description[:65]
+
 
 admin.site.register(models.ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(models.Article, ArticleAdmin)
--- Article_Module/models.py
+++ Article_Module/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 # Create your models here.
 
@@ -11,6 +13,29 @@ class ArticleCategory(models.Model):
     class Meta:
         verbose_name = 'دسته بندی مقاله'
         verbose_name_plural = 'دسته بندی های مقاله'
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    title = models.CharField(max_length = 200, verbose_name = 'عنوان مقاله')
+    slug = models.SlugField(max_length = 400, db_index = True, allow_unicode = True, verbose_name = 'عنوان در url')
+    image = models.ImageField(upload_to = 'images/articles', verbose_name = 'تصویر مقاله')
+    short_description = models.TextField(verbose_name = 'توضیحات کوتاه')
+    text = models.TextField(verbose_name = 'متن مقاله')
+    is_active = models.BooleanField(default = True, verbose_name = 'فعال / غیرفعال')
+    selected_categories = models.ManyToManyField(to = ArticleCategory, verbose_name = 'دسته بندی ها')
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
+
+    def get_selected_categories(self):
+        return "\n - ".join([p.title for p in self.selected_categories.all()])
+
+    def save( self , *args , **kwargs ):
+        self.slug = slugify(self.title)
+        super().save(*args , **kwargs)
 
     def __str__(self):
         return self.title

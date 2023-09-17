591e7f15c4588b10e043baf453317f0f8e48ef53
itz-amethyst <pransermi@gmail.com>
2023-09-17 13:06:54 +0330

    feat: Added jalali date

--- EShop_Project/settings.py
+++ EShop_Project/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'Site_Module',
     # external apps
     'django_render_partial',
-    'sorl.thumbnail'
+    'sorl.thumbnail',
+    'jalali_date'
 
 ]
 
@@ -156,3 +157,28 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'pransermi@gmail.com'
 EMAIL_HOST_PASSWORD = 'fygrifnzzojkwlse'
 EMAIL_PORT = 587
+
+# default settings (optional)
+JALALI_DATE_DEFAULTS = {
+   'Strftime': {
+        'date': '%y/%m/%d',
+        'datetime': '%H:%M:%S _ %y/%m/%d',
+    },
+    'Static': {
+        'js': [
+            # loading datepicker
+            'admin/js/django_jalali.min.js',
+            # OR
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.core.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/calendar.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc.js',
+            # 'admin/jquery.ui.datepicker.jalali/scripts/jquery.ui.datepicker-cc-fa.js',
+            # 'admin/js/main.js',
+        ],
+        'css': {
+            'all': [
+                'admin/jquery.ui.datepicker.jalali/themes/base/jquery-ui.min.css',
+            ]
+        }
+    },
+}

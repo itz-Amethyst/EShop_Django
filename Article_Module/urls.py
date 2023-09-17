7ccc3d5a7b35de3be6e5707c069cc6c4002e8aff
itz-amethyst <pransermi@gmail.com>
2023-09-17 14:00:09 +0330

    feat: Configured Article_category view

--- Article_Module/urls.py
+++ Article_Module/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticlesView.as_view(), name='articles_list_page'),
+    path('cat/<str:category>' , views.ArticlesView.as_view() , name = 'articles_by_category_list') ,
+
 ]
--- Article_Module/views.py
+++ Article_Module/views.py
@@ -1,8 +1,9 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import ListView
 
-from Article_Module.models import Article
+from Article_Module.models import Article , ArticleCategory
 from jalali_date import date2jalali , datetime2jalali
 
 
@@ -24,12 +25,24 @@ class ArticlesView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
+        data = []
         base_query = super().get_queryset()
         data = base_query.filter(is_active = True)
+        category_name = self.kwargs.get('category')
+        if category_name is not None:
+            data = base_query.filter(selected_categories__url_title__iexact = category_name)
         return data
 
     def get_context_data(self,  *args, **kwargs):
         context = super(ArticlesView, self).get_context_data(*args, **kwargs)
         # context['date'] = datetime2jalali(self.request.user.date_joined).strftime('%y/%m/%d _ %H:%M:%S')
-        context['date'] = datetime2jalali(self.request.user.date_joined)
+        # context['date'] = datetime2jalali(self.request.user.date_joined)
         return context
+
+
+def Article_Categories_Partial(request: HttpRequest):
+    article_main_categories = ArticleCategory.objects.filter(is_active = True, parent_id = None)
+    context = {
+        'main_categories': article_main_categories
+    }
+    return render(request, 'Article_Module/partials/article_categories.html', context)

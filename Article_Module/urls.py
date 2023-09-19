c64b96c00b9237f464e156bf2980785454500d99
itz-amethyst <pransermi@gmail.com>
2023-09-19 23:09:13 +0330

    feat: Added Slug to url article view

--- Article_Module/urls.py
+++ Article_Module/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.ArticlesView.as_view(), name='articles_list_page'),
     path('cat/<str:category>' , views.ArticlesView.as_view(), name = 'articles_by_category_list'),
-    path('<pk>/<title>', views.ArticleDetailView.as_view(), name = 'article_detail_page'),
+    path('<pk>/<slug:slug>', views.ArticleDetailView.as_view(), name = 'article_detail_page'),
 
 ]

4dddf62ae225b942fbbba6cec168b457f81b4f70
itz-amethyst <pransermi@gmail.com>
2023-09-19 23:32:38 +0330

    feat: Added about_me to user model

--- Account_Module/models.py
+++ Account_Module/models.py
@@ -4,8 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     # mobile = models.CharField(max_length = 15, verbose_name = "تلفن همراه", null = True)
-    avatar = models.ImageField(upload_to = 'UserImages', verbose_name = 'تصویر کاربری', null = True, blank = True)
+    avatar = models.ImageField(upload_to = 'images/UserImages', verbose_name = 'تصویر کاربری', null = True, blank = True)
     email_active_code = models.CharField(max_length = 100, verbose_name = "کد فعال سازی ایمیل")
+    about_user = models.TextField(null = True, blank = True, verbose_name = 'درباره شخص')
 
     class Meta:
         verbose_name = 'کاربر'
@@ -13,7 +14,7 @@ class User(AbstractUser):
 
     def __str__(self):
         #! Have 2 way to fix this 1 by this long or change it inside a get_full_name method
-        if self.first_name is not '' and self.last_name is not '':
+        if self.first_name != '' and self.last_name != '':
             return self.get_full_name()
 
         return self.email

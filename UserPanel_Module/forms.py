1722637b9e9998ca8022546e44af1d55927daefe
itz-amethyst <pransermi@gmail.com>
2023-09-22 17:00:22 +0330

    feat: Added form for editprofile

---
+++ UserPanel_Module/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from Account_Module.models import User
+
+class EditProfileModelForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'avatar', 'address']
+
+        widgets = {
+            'first_name': forms.TextInput(attrs = {
+                'class': 'form-control',
+                'placeholder': 'نام'
+            }),
+            'last_name': forms.TextInput(attrs = {
+                'class': 'form-control',
+                'placeholder': 'نام خانوادگی'
+            }),
+            'avatar': forms.FileInput(attrs = {
+                'class': 'form-control',
+            }),
+            'address': forms.Textarea(attrs = {
+                'class': 'form-control',
+                'rows': 3,
+                'id': 'message',
+                'placeholder': 'آدرس'
+            })
+        }
+
+        labels = {
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+            'avatar': 'تصویر پروفایل',
+            'address': 'آدرس'
+        }
+
+        error_messages = {
+            'full_name': {
+                'required': 'لطفا نام و نام خواندگی را وارد کنید'
+            },
+            'email': {
+                'required': 'لطفا ایمیل را وارد کنید'
+            },
+            'title': {
+                'required': 'لطفا عنوان را وارد کنید'
+            },
+            'message': {
+                'required': 'لطفا متن پیام را وارد کنید'
+            },
+        }
+
+        # fields = '__all__'
+        # exclude = ['response'] will show all except response

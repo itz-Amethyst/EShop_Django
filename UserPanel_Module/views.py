567123d6ea3b60a50486ef4cb0e76c5842d6e1dd
itz-amethyst <pransermi@gmail.com>
2023-09-22 18:53:08 +0330

    feat: Added changepassword view

--- UserPanel_Module/views.py
+++ UserPanel_Module/views.py
@@ -1,10 +1,12 @@
+from django.contrib.auth import logout
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 
 from Account_Module.models import User
-from UserPanel_Module.forms import EditProfileModelForm
+from UserPanel_Module.forms import EditProfileModelForm , ChangePasswordForm
 
 
 # Create your views here.
@@ -33,6 +35,30 @@ class EditUserProfile(View):
         }
 
         return render(request, 'UserPanel_Module/EditProfile.html', {})
+
+class ChangePassword(View):
+    def get(self, request:HttpRequest):
+        context = {
+            'form': ChangePasswordForm
+        }
+        return render(request, 'UserPanel_Module/ChangePassword.html', context)
+    def post(self, request: HttpRequest):
+        form = ChangePasswordForm(request.POST)
+        if form.is_valid():
+            current_user: User = GetCurrentUser(request)
+            if current_user.check_password(form.cleaned_data.get('current_password')):
+                current_user.set_password(form.cleaned_data.get('password'))
+                current_user.save()
+                logout(request)
+                return redirect(reverse('login_page'))
+            else:
+                form.add_error('password', 'کلمه عبور وارد شده اشتباه میباشد')
+
+
+        context = {
+            'form': form
+        }
+        return render(request , 'UserPanel_Module/ChangePassword.html', context)
 def UserPanelMenuComponent(request: HttpRequest):
     current_user = GetCurrentUser(request)
     context = {

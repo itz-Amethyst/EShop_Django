3a4f8b810698b7c5c9ac0404d681a261b87d9e1a
itz-amethyst <pransermi@gmail.com>
2023-09-22 17:00:14 +0330

    feat: Added EditProfile page

--- UserPanel_Module/urls.py
+++ UserPanel_Module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.UserPanelDashboardClass.as_view(), name='UserPanel_Dashboard')
+    path('', views.UserPanelDashboardClass.as_view(), name='UserPanel_Dashboard'),
+    path('edit-profile', views.EditUserProfile.as_view(), name = 'EditProfile_page')
+
 ]
--- UserPanel_Module/views.py
+++ UserPanel_Module/views.py
@@ -1,8 +1,24 @@
+from django.http import HttpRequest
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
+
+from UserPanel_Module.forms import EditProfileModelForm
 
 
 # Create your views here.
 
 class UserPanelDashboardClass(TemplateView):
     template_name = 'UserPanel_Module/UserPanelDashboard.html'
+
+class EditUserProfile(View):
+    def get( self, request:HttpRequest ):
+        edit_form = EditProfileModelForm
+        context = {
+            'form': edit_form
+        }
+        return render(request, 'UserPanel_Module/EditProfile.html', context)
+    def post( self, request:HttpRequest ):
+        return render(request, 'UserPanel_Module/EditProfile.html', {})
+def UserPanelMenuComponent(request: HttpRequest):
+    return render(request, 'UserPanel_Module/components/UserSidebar.html')

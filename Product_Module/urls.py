4efd040153f04ca5b6bde490bbeec1c9d034ffe9
itz-amethyst <pransermi@gmail.com>
2023-09-23 22:41:59 +0330

    feat: configured product brand url

--- Product_Module/urls.py
+++ Product_Module/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     # path('', views.Product_List, name='product-list'),
     path('', views.ProductListView.as_view(), name = 'product-list'),
-    path('cat/<cat>', views.ProductListView.as_view() , name = 'product-categories-list'),
+    path('cat/<cat>', views.ProductListView.as_view(), name = 'product-categories-list'),
+    path('brand/<brand>' , views.ProductListView.as_view(), name = 'products-brand-list'),
     path('product_favorite', views.AddProductFavoriteView.as_view(), name = 'product-favorite'),
     # path('<slug:slug>', views.ProductDetailView.as_view(), name='product-detail'),
     # re_path(r'products/(?P<slug>[-\w]+)/', views.ProductDetailView.as_view(), name='product-detail'),

789556bfc617345103963fff03713a864f5d6b1e
itz-amethyst <pransermi@gmail.com>
2023-09-25 13:55:27 +0330

    feat: Added some custom scripts

--- static/js/custom.js
+++ static/js/custom.js
@@ -24,3 +24,19 @@ function fillParentId(parentId) {
     $('#parent_id').val(parentId)
     document.getElementById('comment_form_scroll').scrollIntoView({behavior:"smooth"})
 }
+
+function FilterProducts(){
+    const filterPrice = $('#sl2').val()
+    const start_price = filterPrice.split(',')[0]
+    const end_price = filterPrice.split(',')[1]
+
+    $('#start_price').val(start_price)
+    $('#end_price').val(end_price)
+
+    $('#filter_form').submit()
+}
+
+function FillPage(page){
+    $('#page').val(page)
+    $('#filter_form').submit()
+}

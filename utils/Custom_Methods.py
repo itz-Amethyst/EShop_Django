6f66ede01b4b93106c9adedfdca8d4680fbcf7c7
itz-amethyst <pransermi@gmail.com>
2023-09-26 18:37:09 +0330

    feat: Added new custom method

--- utils/Custom_Methods.py
+++ utils/Custom_Methods.py
@@ -3,3 +3,12 @@ from Site_Module.models import SiteBanner
 
 def SelectSiteBanner(choice):
     return SiteBanner.objects.filter(is_active = True, position__iexact = choice)
+
+
+def Group_List(custom_list, size=4):
+    grouped_list = []
+    group_size = size
+
+    for i in range(0, len(custom_list), size):
+        grouped_list.append(custom_list[i:i + size])
+    return grouped_list

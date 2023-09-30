8c17e0735a14e38051f34d5779f95fd66c818693
itz-amethyst <pransermi@gmail.com>
2023-09-30 19:21:25 +0330

    fix

--- static/js/custom.js
+++ static/js/custom.js
@@ -47,7 +47,8 @@ function ShowLargeImageProduct(imageSrc){
 }
 
 function AddProductToOrder(productId){
-    $.get('/order/add-to-order?product_id=' + productId).then(res=>{
+    const productCount = $("#product_count").val()
+    $.get('/order/add-to-order?product_id=' + productId + "&count=" + productCount).then(res=>{
         console.log(res)
     })
 }

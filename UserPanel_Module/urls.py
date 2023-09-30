cde14fe2371da258e0e5d8025ffca5e499c8794b
itz-amethyst <pransermi@gmail.com>
2023-09-30 20:55:20 +0330

    feat: Added basket url and page

--- UserPanel_Module/urls.py
+++ UserPanel_Module/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.UserPanelDashboardClass.as_view(), name='UserPanel_Dashboard'),
     path('edit-profile', views.EditUserProfile.as_view(), name = 'EditProfile_page'),
-    path('change-password', views.ChangePassword.as_view(), name = 'ChangePassword_page')
+    path('change-password', views.ChangePassword.as_view(), name = 'ChangePassword_page'),
+    path('user-basket', views.User_Basket, name = 'user_basket_page')
 
 ]

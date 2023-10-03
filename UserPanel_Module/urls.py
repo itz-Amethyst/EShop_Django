bbf90e9ece3c2573abfcda0b9a732dfcafeba6ca
itz-amethyst <pransermi@gmail.com>
2023-10-03 13:14:11 +0330

    feat: added some features

--- UserPanel_Module/urls.py
+++ UserPanel_Module/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.UserPanelDashboardClass.as_view(), name='UserPanel_Dashboard'),
     path('edit-profile', views.EditUserProfile.as_view(), name = 'EditProfile_page'),
     path('change-password', views.ChangePassword.as_view(), name = 'ChangePassword_page'),
-    path('user-basket', views.User_Basket, name = 'user_basket_page')
+    path('user-basket', views.User_Basket, name = 'user_basket_page'),
+    path('remove-item-basket', views.remove_order_detail, name = 'remove_item_in_basket_ajax')
 
 ]

b805b9560606c2bca943ec01ee47eb43ee6fd0c7
itz-amethyst <pransermi@gmail.com>
2023-10-03 13:15:02 +0330

    feat: Filled remove order def in view

--- UserPanel_Module/views.py
+++ UserPanel_Module/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest
+from django.http import HttpRequest , JsonResponse
 from django.shortcuts import render , redirect
+from django.template.loader import render_to_string
 from django.urls import reverse
 from django.views import View
 from django.views.generic import TemplateView
 
 from Account_Module.models import User
-from Order_Module.models import Order
+from Order_Module.models import Order , OrderDetail
 from UserPanel_Module.forms import EditProfileModelForm , ChangePasswordForm
 
 
@@ -75,16 +76,56 @@ def UserPanelMenuComponent( request: HttpRequest ):
     }
     return render(request , 'UserPanel_Module/components/UserSidebar.html' , context)
 
-def User_Basket(request: HttpRequest):
-    current_order, created = Order.objects.prefetch_related('orderdetail_set').get_or_create(is_paid = False, user_id = request.user.id)
+
+def User_Basket( request: HttpRequest ):
+    current_order, created = Order.objects.prefetch_related('orderdetail_set').get_or_create(is_paid = False ,
+                                                                                              user_id = request.user.id)
     total_amount = 0
 
     for order_detail in current_order.orderdetail_set.all():
         total_amount += order_detail.product.price * order_detail.count
 
+    context = {
+        'order': current_order ,
+        'sum': total_amount
+    }
+    return render(request , 'UserPanel_Module/user_basket.html' , context)
+
+
+def remove_order_detail(request: HttpRequest):
+
+    detail_id = request.GET.get('detail_id')
+    if detail_id is None:
+
+        return JsonResponse({
+            'status': 'not_found_detail_id'
+        })
+
+    deleted_count, deleted_dict = OrderDetail.objects.filter(id=detail_id, order__is_paid=False, order__user_id=request.user.id).delete()
+
+    if deleted_count == 0:
+        return JsonResponse({
+            'status': 'detail_not_found'
+        })
+
+
+    current_order, created = Order.objects.prefetch_related('orderdetail_set').get_or_create(is_paid = False, user_id = request.user.id)
+    # Todo:
+    total_amount = current_order.calculate_total_price()
+
+    # detail = current_order.orderdetail_set.filter(id = detail_id).first()
+    # detail.delete()
+
+
+    # for order_detail in current_order.orderdetail_set.all().exclude(id=detail_id):
+    #     total_amount += order_detail.product.price * order_detail.count
 
     context = {
         'order': current_order,
         'sum': total_amount
     }
-    return render(request, 'UserPanel_Module/user_basket.html', context)
+
+    return JsonResponse({
+        'status': 'success',
+        'body': render_to_string('UserPanel_Module/includes/user_basket_content.html', context)
+    })

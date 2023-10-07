ffbec2b744fae9d6e8ad21a0ef42cca0a3680e80
itz-amethyst <pransermi@gmail.com>
2023-10-07 14:28:20 +0330

    fix

--- UserPanel_Module/urls.py
+++ UserPanel_Module/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('edit-profile', views.EditUserProfile.as_view(), name = 'EditProfile_page'),
     path('change-password', views.ChangePassword.as_view(), name = 'ChangePassword_page'),
     path('user-basket', views.User_Basket, name = 'user_basket_page'),
-    path('remove-item-basket', views.remove_order_detail, name = 'remove_item_in_basket_ajax')
+    path('remove-item-basket', views.remove_order_detail, name = 'remove_item_in_basket_ajax'),
+    path('change-item-count', views.Change_Order_Count, name = 'change_item_count_ajax'),
 
 ]

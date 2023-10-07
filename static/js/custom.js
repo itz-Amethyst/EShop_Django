f385cfc2c0e30aa1bac296964048a7f88dc1b07f
itz-amethyst <pransermi@gmail.com>
2023-10-07 14:27:48 +0330

    feat: Added ajax decrease and increase

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,31 +1,30 @@
-function sendArticleComment(articleId){
+function sendArticleComment(articleId) {
     var comment = $('#commentText').val()
     var parentId = $('#parent_id').val()
     // ajx => asynchronous javascript and xml
     $.get('/articles/add-article-comment', {
         articleComment: comment,
-        articleId : articleId,
+        articleId: articleId,
         parentId: parentId
     }).then(res => {
         $('#comment_response').html(res)
         $('#commentText').val('')
         $('#parent_id').val('')
         //! Note: Comment must be submitted by admin so these part is unusefull
-        if(parentId !== null && parentId !== ''){
-            document.getElementById('single_comment_box_' + parentId).scrollIntoView({behavior:"smooth"})
-        }
-        else{
-            document.getElementById('comment_response').scrollIntoView({behavior:"smooth"})
+        if (parentId !== null && parentId !== '') {
+            document.getElementById('single_comment_box_' + parentId).scrollIntoView({behavior: "smooth"})
+        } else {
+            document.getElementById('comment_response').scrollIntoView({behavior: "smooth"})
         }
     });
 }
 
 function fillParentId(parentId) {
     $('#parent_id').val(parentId)
-    document.getElementById('comment_form_scroll').scrollIntoView({behavior:"smooth"})
+    document.getElementById('comment_form_scroll').scrollIntoView({behavior: "smooth"})
 }
 
-function FilterProducts(){
+function FilterProducts() {
     const filterPrice = $('#sl2').val()
     const start_price = filterPrice.split(',')[0]
     const end_price = filterPrice.split(',')[1]
@@ -36,34 +35,42 @@ function FilterProducts(){
     $('#filter_form').submit()
 }
 
-function FillPage(page){
+function FillPage(page) {
     $('#page').val(page)
     $('#filter_form').submit()
 }
 
-function ShowLargeImageProduct(imageSrc){
+function ShowLargeImageProduct(imageSrc) {
     $('#main_image').attr('src', imageSrc)
     $('#show_large_image_modal').attr('href', imageSrc)
 }
 
-function AddProductToOrder(productId){
+function AddProductToOrder(productId) {
     const productCount = $("#product_count").val()
-    $.get('/order/add-to-order?product_id=' + productId + "&count=" + productCount).then(res=>{
+    $.get('/order/add-to-order?product_id=' + productId + "&count=" + productCount).then(res => {
         Swal.fire({
-                title: "اعلان",
-                text: res.text,
-                icon: res.icon,
-                confirmButtonText: res.confirm_button_text
-            }).then(confirm =>{
-                if (res.status === 'not_auth'){
-                    window.location.href = '/login'
-                }
+            title: "اعلان",
+            text: res.text,
+            icon: res.icon,
+            confirmButtonText: res.confirm_button_text
+        }).then(confirm => {
+            if (res.status === 'not_auth') {
+                window.location.href = '/login'
+            }
         })
     })
 }
 
-function Remove_Item_In_OrderDetail(detailId){
-    $.get('/user/remove-item-basket?detail_id=' + detailId).then(res =>{
+function Remove_Item_In_OrderDetail(detailId) {
+    $.get('/user/remove-item-basket?detail_id=' + detailId).then(res => {
+        if (res.status === 'success') {
+            $('#order-detail-content').html(res.body)
+        }
+    })
+}
+
+function Change_Order_Detail_Count(detailId, state) {
+    $.get('/user/change-item-count?detail_id=' + detailId + '&state=' + state).then(res => {
         if (res.status === 'success') {
             $('#order-detail-content').html(res.body)
         }

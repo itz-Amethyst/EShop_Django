960506cc84eb077b2924a9c4bb9fe5855b2b2341
itz-amethyst <pransermi@gmail.com>
2023-10-08 17:52:10 +0330

    feat: Added new urls

--- Order_Module/urls.py
+++ Order_Module/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-to-order', views.Add_Product_To_Order, name='add_product_to_order')
+    path('add-to-order', views.Add_Product_To_Order, name='add_product_to_order'),
+    path('request-payment', views.Request_Payment, name='request_Payment'),
+    path('verify-payment', views.Verify_Payment, name='verify_Payment'),
 ]

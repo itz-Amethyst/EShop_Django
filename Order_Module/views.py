c9fa7a27e00bae6a9efa967a6aeb1564dace58e4
itz-amethyst <pransermi@gmail.com>
2023-10-09 19:43:10 +0330

    fixed: Methods in order_module view

--- Order_Module/views.py
+++ Order_Module/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest , HttpResponse , JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.urls import reverse
 
 from Order_Module.models import Order , OrderDetail
 from Product_Module.models import Product
@@ -26,7 +28,7 @@ def Add_Product_To_Order( request: HttpRequest ):
         if product != None:
 
             # Cause its return tuple [any , bool]
-            current_order , created = Order.objects.get_or_create(is_paid = False , user_id = request.user.id)
+            current_order, created = Order.objects.get_or_create(is_paid = False , user_id = request.user.id)
             current_order_detail = current_order.orderdetail_set.filter(product_id = product_id).first()
             if current_order_detail != None:
                 current_order_detail.count += count
@@ -58,10 +60,31 @@ def Add_Product_To_Order( request: HttpRequest ):
         })
 
 
-def Request_Payment( request: HttpRequest ):
-    Send_Request(request)
+@login_required
+def Request_Payment(request: HttpRequest):
+
+    current_order, created = Order.objects.get_or_create(is_paid = False , user_id = request.user.id)
+    total_price = current_order.calculate_total_price()
+    if total_price == 0:
+        return redirect(reverse('user_basket_page'))
+
+    return Send_Request(request, total_price)
 
 
+@login_required
 def Verify_Payment(request: HttpRequest):
+    current_order, created = Order.objects.get_or_create(is_paid = False, user_id = request.user.id)
+    total_price = current_order.calculate_total_price()
+    if total_price == 0:
+        return redirect(reverse('user_basket_page'))
     t_authority = request.GET['Authority']
-    Verify(t_authority)
+    data = Verify(t_authority, total_price)
+    # TODO: later fix this to apply is paid in here something with this refer to video
+    if data['info']:
+        print('yes')
+    elif data['success']:
+        print('no')
+    else:
+        print("Nobody")
+    return render(request, 'Order_Module/Payment_Result.html', data)
+

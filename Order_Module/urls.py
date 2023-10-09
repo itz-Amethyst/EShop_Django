a2fda55e4af832d20cc332372c65c1b933d9c3c0
itz-amethyst <pransermi@gmail.com>
2023-10-09 19:43:21 +0330

    fix

--- Order_Module/urls.py
+++ Order_Module/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add-to-order', views.Add_Product_To_Order, name='add_product_to_order'),
-    path('request-payment', views.Request_Payment, name='request_Payment'),
-    path('verify-payment', views.Verify_Payment, name='verify_Payment'),
+    path('request-payment/', views.Request_Payment, name='request_Payment'),
+    path('verify-payment/', views.Verify_Payment, name='verify_Payment')
 ]

680f615fd19aeb6bb707dbbd3dcb55cf1b319619
itz-amethyst <pransermi@gmail.com>
2023-10-14 21:21:24 +0330

    feat: Added article_list view

--- Admin_Panel/urls.py
+++ Admin_Panel/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('dashboard/', views.Index, name='Admin_Dashboard'),
-    path('article-lists/', views.All_Articles, name='Admin_Articles'),
+    path('article-lists/', views.ArticlesView.as_view(), name='Admin_Articles'),
 ]
--- Admin_Panel/views.py
+++ Admin_Panel/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from Article_Module.models import Article
+
 
 # Create your views here.
 
@@ -10,3 +14,27 @@ def Index(request: HttpRequest):
 
 def All_Articles(request: HttpRequest):
     return render(request, 'Admin_Panel/Articles/Articles_List.html', {})
+
+
+
+class ArticlesView(ListView):
+    template_name = 'Admin_Panel/Articles/Articles_List.html'
+    model = Article
+    context_object_name = 'Article_Lists'
+    # ordering = ['-created_date']
+    paginate_by = 7
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        # base_query: Article = base_query.selected_categories
+        # category_name = self.kwargs.get('category')
+        # if category_name is not None:
+        #     base_query = base_query.filter(selected_categories__url_title__iexact = category_name)
+        return base_query
+
+    def get_context_data(self,  *args, **kwargs):
+        context = super(ArticlesView, self).get_context_data(*args, **kwargs)
+        # context['date'] = datetime2jalali(self.request.user.date_joined).strftime('%y/%m/%d _ %H:%M:%S')
+        # context['date'] = datetime2jalali(self.request.user.date_joined)
+        return context
+
